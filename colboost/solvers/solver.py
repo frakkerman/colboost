5ac894b2f661c2ef49f742230c54b72902eb7242
frakkerman <f.r.akkerman@utwente.nl>
2025-06-24 11:34:59 +0200

    cleanup

--- colboost/solvers/solver.py
+++ colboost/solvers/solver.py
@@ -1,35 +1,3 @@
-# from abc import ABC, abstractmethod
-# from typing import Any
-# from gurobipy import Env
-#
-#
-# class Solver(ABC):
-#     """
-#     Abstract base class for solvers assigning ensemble weights.
-#     """
-#
-#     def __init__(self):
-#         self.env = Env(params={"LogFile": ""})
-#
-#     def __del__(self):
-#         if hasattr(self, "env"):
-#             try:
-#                 self.env.dispose()
-#             except Exception:
-#                 pass
-#
-#     @abstractmethod
-#     def solve(self, args: object, data_train: object, env: object) -> Any:
-#         """
-#         Run the solver and return updated weights, duals, objective, and timing.
-#
-#         Must be implemented by subclasses.
-#         """
-#         raise NotImplementedError(
-#             "Subclasses must implement the solve() method."
-#         )
-
-
 import numpy as np
 from abc import ABC, abstractmethod
 from typing import Any, Optional
